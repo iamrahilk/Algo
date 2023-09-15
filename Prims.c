d4d0db25221711695d7bad6f01e272c37dd9c598
iamrahilk <rahilk3483@icloud.com>
2023-09-15 13:29:05 -0400

    .

--- Prims.c
+++ Prims.c
@@ -7,11 +7,11 @@ void Prims_Algorithm(int[]);
 int main()
 {
     int i,j,Graph[V][V]={
-        {},
-        {},
-        {},
-        {},
-        {}
+        {0,3,5,2,1},
+        {4,0,8,6,3},
+        {8,4,0,6,1},
+        {7,3,6,0,9},
+        {4,2,6,1,0}
     }
     Prims_Algorithm(Graph);
 }
