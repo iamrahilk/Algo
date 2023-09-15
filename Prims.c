7344c6c0550a7712a8208d00aba2cd384ecc2424
iamrahilk <rahilk3483@icloud.com>
2023-09-15 11:03:05 -0400

    .

--- Prims.c
+++ Prims.c
@@ -1,7 +1,8 @@
 #include<limits.h>
 #include<stdbool.h>
 #include<stdio.h>
-#define V 5 //No Of Vertices(Nodes)
+#define V 5 /*No Of Vertices(Nodes)*/
+void Prims_Algorithm(int[]);
 
 int main()
 {
@@ -13,4 +14,7 @@ int main()
         {}
     }
     Prims_Algorithm(Graph);
+}
+void Prims_Algorithm(){
+
 }
