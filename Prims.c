7697d8e69097f51a718f9de56d4dc171df6f246d
iamrahilk <rahilk3483@icloud.com>
2023-09-15 09:46:12 -0400

    .

---
+++ Prims.c
@@ -0,0 +1,15 @@
+#include<limits.h>
+#include<stdbool.h>
+#include<stdio.h>
+#define V 5
+
+main()
+{
+    int i,j,graph[V][V]={
+        {},
+        {},
+        {},
+        {},
+        {}
+    }
+}
